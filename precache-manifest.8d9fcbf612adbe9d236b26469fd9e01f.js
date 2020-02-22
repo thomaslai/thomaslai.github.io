65ff6700c84696505a89528a2d02107b2265f616
Thomas Lai <thomaslai@users.noreply.github.com>
2020-02-22 14:42:45 -0800

    Updates

---
+++ precache-manifest.8d9fcbf612adbe9d236b26469fd9e01f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1bf689006fba24d93992f55b02c8915",
+    "url": "/socratic-graph/index.html"
+  },
+  {
+    "revision": "d2a67159c56ddc338cb3",
+    "url": "/socratic-graph/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "c3e19b326c462c706e35",
+    "url": "/socratic-graph/static/js/2.fb3e362f.chunk.js"
+  },
+  {
+    "revision": "5a8eee8a6f77926e9e590cfc4e41e8a1",
+    "url": "/socratic-graph/static/js/2.fb3e362f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2a67159c56ddc338cb3",
+    "url": "/socratic-graph/static/js/main.3045cac6.chunk.js"
+  },
+  {
+    "revision": "d7a4ecab9eed34e2facf",
+    "url": "/socratic-graph/static/js/runtime-main.b82af8aa.js"
+  }
+]);
